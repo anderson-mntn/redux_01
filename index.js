0d29568ab4092ee76b6b6d411ce8bd987a74140d
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-06-12 10:08:35 -0300

    colocando payload na action

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 
-const incrementAction = {type: 'INCREMENT'}
-const decrementAction = {type: 'DECREMENT'}
+const incrementAction = (value) => { return {type: 'INCREMENT', payload: value || 1 } }
+const decrementAction = (value) => { return {type: 'DECREMENT', payload: value || 1 } }
 
 function counterReducer(state = 3, action){
 
     switch (action.type) {
-        case 'INCREMENT': return state + 1;
+        case 'INCREMENT': return state + action.payload;
          break;
-        case 'DECREMENT' : return state - 1;
+        case 'DECREMENT' : return state - action.payload;
          break
         default: 
          return state;
@@ -22,4 +22,11 @@ console.log(store.getState())
 
 store.subscribe(()=>{console.log(store.getState())}) // .subscribe() é ativado ao mudar estado
 
-store.dispatch(incrementAction) // .dispacth() levará a ação(argumento) que será levada para o reducer.  
+// store.dispatch(incrementAction) // .dispacth() levará a ação(argumento) que será levada para o reducer.  
+
+// --- passando incrementAction com carga/payload ---
+
+store.dispatch(incrementAction())
+store.dispatch(incrementAction(1))
+store.dispatch(incrementAction(5))
+store.dispatch(decrementAction(3))
